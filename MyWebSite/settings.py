05d230961147b9832593cb1af48f6b8546ea0ec0
Tenku876 <poyo2c@gmail.com>
2019-02-02 14:35:55 +0900

    aaa

--- MyWebSite/settings.py
+++ MyWebSite/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# python-decoupleを使用　参照：
+# https://wonderwall.hatenablog.com/entry/2018/04/06/001500
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
