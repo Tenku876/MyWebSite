a72dbd923c121daab4ad946bde40d5a7686b1ef3
Tenku876 <poyo2c@gmail.com>
2019-02-21 22:14:09 +0900

    Change top page.

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def render_index(request):
+    my_list = {
+        "app": "Django"
+    }
+    return render(request, 'index.html', my_list)
