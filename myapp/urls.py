e695b215601f536fd192055fcf755b7618dd921a
Tenku876 <poyo2c@gmail.com>
2019-02-21 23:03:02 +0900

    static sono2

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.render_index, name='render_index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
